347fecd614a8325d6bf0f718e16a3e5e29c9e64c
storm345 <storm345dev@gmail.com>
2013-06-16 20:55:28 +0100

    Started on options

--- src/com/stormdev/minigamez/editor/WindowArea.java
+++ src/com/stormdev/minigamez/editor/WindowArea.java
@@ -3,6 +3,7 @@ package com.stormdev.minigamez.editor;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -34,9 +35,10 @@ import javax.swing.JSplitPane;
 
 public class WindowArea extends JFrame implements ActionListener,ChangeListener {
 	JPanel pane = new JPanel();
-	int numberOfOptions = 1; //TODO ALWAYS update this
-	JPanel optionsPane = new JPanel(new GridLayout(numberOfOptions,1));
+	//int numberOfOptions = 1; //TODO ALWAYS update this
+	JPanel optionsPane = new JPanel(new GridLayout());
 	JPanel optionSettingsPane = new JPanel();
+	JLabel spacer = new JLabel(" ");
 	  JLabel result = new JLabel();
 	  JButton saveButton = new JButton();
 	  JButton loadButton = new JButton();
@@ -49,8 +51,9 @@ public class WindowArea extends JFrame implements ActionListener,ChangeListener 
 	  private JScrollPane scrollPane_1 = new JScrollPane(optionSettingsPane);
 	  private final JButton btnPlayerCount = new JButton("Player count");
 	  private final JButton savePlayerCount = new JButton("Save!");
-	  private JPanel optionsPlayerCount = new JPanel(new GridLayout(10,1));
+	  private JPanel optionsPlayerCount = new JPanel(new GridLayout(0,2));
 	  JTextField minPlayers = new JTextField(16);
+	  JTextField maxPlayers = new JTextField(16);
 	  WindowArea() // the frame constructor method
 	  {
 	    super("Minigame Creator"); 
@@ -97,10 +100,12 @@ public class WindowArea extends JFrame implements ActionListener,ChangeListener 
 	    playerCountOptionsTitle.setFont(title);
 	    minPlayers.setText("2");
 	    minPlayers.addActionListener(this);
-	    optionsPlayerCount.add(playerCountOptionsTitle);
-	    optionsPlayerCount.add(new JLabel("Minimum players:"));
-	    optionsPlayerCount.add(minPlayers);
-	    optionsPlayerCount.add(savePlayerCount);
+	    maxPlayers.setText("4");
+	    maxPlayers.addActionListener(this);
+	    optionsPlayerCount.add(playerCountOptionsTitle); optionsPlayerCount.add(spacer); //optionsPlayerCount.add(spacer);  optionsPlayerCount.add(spacer); 
+	    optionsPlayerCount.add(new JLabel("Minimum players:")); optionsPlayerCount.add(minPlayers); //optionsPlayerCount.add(spacer);
+	    optionsPlayerCount.add(new JLabel("Maximum players:")); optionsPlayerCount.add(maxPlayers); //optionsPlayerCount.add(spacer);
+	    optionsPlayerCount.add(savePlayerCount); //optionsPlayerCount.add(spacer);
 	    //end player count options page
 	  }
 	  
@@ -127,22 +132,43 @@ public class WindowArea extends JFrame implements ActionListener,ChangeListener 
 	    }
 	    if (source == savePlayerCount)
 	    {
-	    System.out.println("Detected change in player count options");
+	    	Boolean valid = true;
+	    System.out.println("Saving change in player count options");
 	      String minP = minPlayers.getText();
 	      int minimum = 2;
 	      try {
 			minimum = Integer.parseInt(minP);
 		} catch (NumberFormatException e1) {
 			minPlayers.setText("2");
-			popUpMsg("Field must be a number!", "ERROR");
+			popUpMsg("Minimum must be a number!", "ERROR");
+			valid = false;
 		}
 	      if(minimum < 1){
 	    	  minPlayers.setText("2");
-				popUpMsg("Field must be at least 1!", "ERROR");
+				popUpMsg("Minimum must be at least 1!", "ERROR");
+				valid = false;
 	      }
-	    }
 	    
-	  }
+	    String maxP = maxPlayers.getText();
+	      int maximum = 2;
+	      try {
+			maximum = Integer.parseInt(maxP);
+		} catch (NumberFormatException e1) {
+			maxPlayers.setText("4");
+			popUpMsg("Maximum must be a number!", "ERROR");
+			valid = false;
+		}
+	      if(maximum < 1){
+	    	  maxPlayers.setText("2");
+				popUpMsg("Maximum must be at least 1!", "ERROR");
+				valid = false;
+	      }
+	    if(!valid){
+	    	return;
+	    }
+	    //save it
+	    }
+      }
 	  public void popUpMsg(String msg, String title){
 		  JOptionPane.showMessageDialog(null,msg,title,
 			      JOptionPane.PLAIN_MESSAGE); setVisible(true);  // show something
