f9f512bbbc9631d7976d3e121d268e08fef91c8b
storm345 <storm345dev@gmail.com>
2013-06-16 11:09:23 +0100

    Fixed resource loading

--- src/com/stormdev/minigamez/editor/CustomGameCreator.java
+++ src/com/stormdev/minigamez/editor/CustomGameCreator.java
@@ -5,12 +5,6 @@ import java.io.InputStream;
 
 public class CustomGameCreator {
 	public static void main(String[] args) {
-		InputStream stream = CustomGameCreator.class.getResourceAsStream("plugin.yml"); //Make sure resource is copied to jar on export
-		try {
-			stream.close();
-		} catch (IOException e) {
-			//Do nothing
-		}
 		System.out.println("Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! Hello world! ");
         return;
 	}
